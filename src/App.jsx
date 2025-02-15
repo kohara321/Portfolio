7c32f1676436eb2ddef5296c9da3865faceaf36f
kohara321 <kohara@DESKTOP-VIORR1O.localdomain>
2025-02-15 14:31:36 -0700

    remove About file, setup section files, linked navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import './App.css'
 import './index.css'
+import { useEffect } from 'react'
 import { Navbar } from './components/Navbar'
+import { Home } from './components/sections/Home'
+import { Contact } from './components/sections/Contact'
+import { Projects } from './components/sections/Projects'
+
 
 function App() {
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      if (window.location.hash === '#home') {
+        window.history.replaceState('', document.title, window.location.pathname + window.location.search);
+      }
+    };
+
+    window.addEventListener('hashchange', handleHashChange);
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  }, []);
 
   return (
     <>
       <Navbar />
-      <div className='min-h-screen w-full relative bg-gradient-to-r from-rose-600 via-indigo-800 to-sky-700 flex items-center justify-center text-center flex-col gap-10 text-white p-52'>
-        <h1 className='text-7xl'>Home Section</h1>
-        <p className='text-3xl'> Lorem ipsum odor amet, consectetuer adipiscing elit. Lobortis platea natoque mauris nec ex cubilia taciti. Eleifend adipiscing sem phasellus laoreet molestie sit. Quis faucibus sit fames sapien; ridiculus nec.</p>
-
-      </div>
-
-      <div className='min-h-screen w-full relative bg-gradient-to-r from-orange-600 via-red-600 to-purple-600 flex items-center justify-center text-center flex-col gap-10 text-white p-52'>
-        <h1 className='text-7xl'>Project Section</h1>
-        <p className='text-3xl'> Lorem ipsum odor amet, consectetuer adipiscing elit. Lobortis platea natoque mauris nec ex cubilia taciti. Eleifend adipiscing sem phasellus laoreet molestie sit. Quis faucibus sit fames sapien; ridiculus nec.</p>
-      </div>
-
-      <div className='min-h-screen w-full relative bg-gradient-to-r from-yellow-500 to-amber-500 flex items-center justify-center text-center flex-col gap-10 text-white p-52'>
-        <h1 className='text-7xl'>Contact Section</h1>
-        <p className='text-3xl'> Lorem ipsum odor amet, consectetuer adipiscing elit. Lobortis platea natoque mauris nec ex cubilia taciti. Eleifend adipiscing sem phasellus laoreet molestie sit. Quis faucibus sit fames sapien; ridiculus nec.</p>
-      </div>
+      <Home />
+      <Projects />
+      <Contact />
     </>
   )
 }
---
+++ src/components/sections/Contact.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Contact = () => {
+    return <section id="contact" className='min-h-screen w-full relative bg-gradient-to-r from-yellow-500 to-amber-500 flex items-center justify-center text-center flex-col gap-10 text-white p-52'>
+
+            <h1 className='text-7xl'>Contact Section</h1>
+            <p className='text-3xl'> Lorem ipsum odor amet, consectetuer adipiscing elit. Lobortis platea natoque mauris nec ex cubilia taciti. Eleifend adipiscing sem phasellus laoreet molestie sit. Quis faucibus sit fames sapien; ridiculus nec.</p>
+
+    </section>
+}
